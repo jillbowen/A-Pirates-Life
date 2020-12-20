c2cb9dfb769ff8c61547d9b511be216a117713b2
jillbowen <jillian.b.short@gmail.com>
2020-12-20 15:24:54 -0800

    add validations

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,5 @@
 class Map < ApplicationRecord
     has_many :buried_treasures
     has_many :pirates, through: :buried_treasures
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,4 +2,6 @@ class Pirate < ApplicationRecord
     has_many :buried_treasures
     has_many :maps, through: :buried_treasures
     has_secure_password
+    validates :name, presence: true, uniqueness: true
+    validates :password, presence :true
 end
