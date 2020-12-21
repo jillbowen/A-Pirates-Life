6a486221bf895998f973a2d09664d91a434a6ad0
jillbowen <jillian.b.short@gmail.com>
2020-12-20 16:59:55 -0800

    fix syntax

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,5 +3,5 @@ class Pirate < ApplicationRecord
     has_many :maps, through: :buried_treasures
     has_secure_password
     validates :name, presence: true, uniqueness: true
-    validates :password, presence :true
+    validates :password, presence: true
 end
