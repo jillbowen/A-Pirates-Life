323845e0750a8404212bf70591d81afe7bcc8db7
jillbowen <jillian.b.short@gmail.com>
2020-12-20 18:32:03 -0800

    remove unnecessary password validation

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,5 +3,4 @@ class Pirate < ApplicationRecord
     has_many :maps, through: :buried_treasures
     has_secure_password
     validates :name, presence: true, uniqueness: true
-    validates :password, presence: true
 end
