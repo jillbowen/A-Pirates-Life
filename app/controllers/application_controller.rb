73ae06844cecc7d4028e19ec91e89fe31e46fa85
jillbowen <jillian.b.short@gmail.com>
2020-12-23 12:21:06 -0800

    add helper methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+    
+    helper_method :current_user, :logged_in?
+    
     def welcome 
+    end
+
+    private
+
+    def logged_in?
+        !!session[:pirate_id]
+    end
+
+    def current_pirate
+        @current_pirate ||= Pirate.find_by_id(session[:pirate_id]) if session[:user_id]
     end
 end
