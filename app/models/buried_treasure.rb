50318548a0bc5cbc5aee8226486f26c42a2bd876
jillbowen <jillian.b.short@gmail.com>
2020-12-25 20:16:41 -0800

    add scope method

--- app/models/buried_treasure.rb
+++ app/models/buried_treasure.rb
@@ -2,4 +2,6 @@ class BuriedTreasure < ApplicationRecord
     has_many :maps
     has_many :pirates, through: :maps
     validates :name, presence: true, uniqueness: true
+
+    scope :least_cursed, -> {order(:curses)}
 end
